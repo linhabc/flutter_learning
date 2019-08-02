211a2a407dc1ef2d68b1943d522ef60636f27892
linhnln <jonathanrocrach2@gmail.com>
2019-08-02 09:05:30 +0700

    using combine reducer instead of many if else

--- to_do_list/lib/redux/reducers.dart
+++ to_do_list/lib/redux/reducers.dart
@@ -1,3 +1,5 @@
+import 'package:redux/redux.dart';
+
 import './actions.dart';
 import '../model/model.dart';
 
@@ -5,20 +7,37 @@ AppState appStateReducer(AppState state, action) {
   return AppState(items: itemReducer(state.items, action));
 }
 
-List<Item> itemReducer(List<Item> state, action) {
-  if (action is AddItemAction) {
-    return []
-      ..addAll(state)
-      ..add(Item(id: action.id, body: action.item));
-  }
+final Reducer<List<Item>> itemReducer = combineReducers([
+  TypedReducer<List<Item>, AddItemAction>(addItem),
+  TypedReducer<List<Item>, RemoveItemAction>(removeItem),
+  TypedReducer<List<Item>, RemoveItemsAction>(removeItems),
+]);
 
-  if (action is RemoveItemAction) {
-    return List.unmodifiable(List.from(state)..remove(action.item));
-  }
-
-  if (action is RemoveItemsAction) {
-    return List.unmodifiable([]);
-  }
-
-  return state;
+List<Item> addItem(List<Item> state, AddItemAction action) {
+  return List.from(state)..add(Item(id: action.id, body: action.item));
 }
+
+List<Item> removeItem(List<Item> state, RemoveItemAction action) {
+  return List.unmodifiable(List.from(state)..remove(action.item));
+}
+
+List<Item> removeItems(List<Item> state, RemoveItemsAction action) {
+  return List.unmodifiable([]);
+}
+
+// This equivalant to above itemReducer
+// List<Item> itemReducer(List<Item> state, action) {
+//   if (action is AddItemAction) {
+//     return List.from(state)..add(Item(id: action.id, body: action.item));
+//   }
+
+//   if (action is RemoveItemAction) {
+//     return List.unmodifiable(List.from(state)..remove(action.item));
+//   }
+
+//   if (action is RemoveItemsAction) {
+//     return List.unmodifiable([]);
+//   }
+
+//   return state;
+// }
